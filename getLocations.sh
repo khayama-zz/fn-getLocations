5b289fb9e4cee0ef71d6bf36cbac7ccb59ae3e65
Kyouhei Hayama <khayama@users.noreply.github.com>
2020-04-29 02:33:58 +0900

    Update getLocations.sh

--- getLocations.sh
+++ getLocations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read ARGS
+ARGS=$@
 IBMCLOUD_API_KEY=`echo "$ARGS" | jq -r '."IBMCLOUD_API_KEY"'`
 ibmcloud login -a cloud.ibm.com -r jp-tok -g Default --apikey $IBMCLOUD_API_KEY
 LOCATIONS=`ibmcloud sl call-api Location getViewablePopsAndDataCenters --mask "longName,name,locationAddress[locationId,address1,address2,city,country,isActive,postalCode,state,type[keyName]]" | jq -c .`
